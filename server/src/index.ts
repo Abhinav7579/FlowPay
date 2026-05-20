f3e4db6c18ef22870a01c370708607b2b6f00fad
Abhinav <abhinavsyunary@gmail.com>
2026-05-20 12:38:04 +0530

    remove env file

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,7 +6,9 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
+
 console.log("hello")
+
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`app running on port ${PORT} `);
