095ba9bd285b670a7bc503a011110e4ea5f1a1ca
Abhinav <abhinavsyunary@gmail.com>
2026-05-23 12:59:54 +0530

    admin api created

---
+++ server/src/route/admin.ts
@@ -0,0 +1,63 @@
+import express from "express"
+const adminRouter=express.Router();
+import client from "../prismaclient.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
+import { roleCheck } from "../middleware/roleMiddleware.js";
+adminRouter.get("/",authMiddleware,roleCheck("ADMIN"),(req,res)=>{
+    res.status(200).json({
+        success:true,
+        message:"Welcome to admin route"
+    })
+});
+
+adminRouter.get("/pending-vendors",authMiddleware,roleCheck("ADMIN"),async(req,res)=>{
+
+    try{
+        const pendingVendors=await client.vendor.findMany({
+            where:{
+                isApproved:false
+            }
+        });
+        res.status(200).json({
+            success:true,
+            message:"List of pending vendors",
+            data:pendingVendors
+        });
+    }
+    catch(error){
+        res.status(500).json({
+            success:false,
+            message:"Internal server error"+error
+        });
+    } 
+})
+
+adminRouter.post("/approve-vendor/:vendorId",authMiddleware,roleCheck("ADMIN"),async(req,res)=>{
+    const vendorId=req.params.vendorId;
+    if (typeof vendorId !== "string") {
+    throw new Error("Invalid vendor id");
+     }
+    try{
+        const vendor=await client.vendor.update({
+            where:{
+                  id:vendorId
+            },
+            data:{
+                isApproved:true
+            }
+        });
+        res.status(200).json({
+            success:true,
+            message:"Vendor approved successfully",
+            data:vendor
+        });
+
+    }
+    catch(error){
+        res.status(500).json({
+            success:false,
+            message:"Internal server error"+error
+        });
+    }
+});
+export default adminRouter;
--- server/src/route/index.ts
+++ server/src/route/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import userRouter from "./user.js";
 import vendorRouter from "./vendor.js";
+import adminRouter from "./admin.js";
 const router = express.Router();
 
 router.use("/user", userRouter);
 router.use("/user/vendor", vendorRouter);
+router.use("/user/admin", adminRouter);
 export default router;
