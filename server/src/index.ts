67efacac29c75a630ffe5250d051fe84af4295db
Abhinav <abhinavsyunary@gmail.com>
2026-06-06 14:20:13 +0530

    port correction

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,7 +9,7 @@ app.use(express.json());
 
 
 app.use("/api/v1", mainRouter);
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`app running on port ${PORT} `);
 });
