2870db64309aa0fa438abe3dec4d0e9f2ef269f3
Abhinav <abhinavsyunary@gmail.com>
2026-06-06 14:58:46 +0530

    final updates done

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
 import mainRouter from "./route/index.js"
+import "./workers/runAll.js";
 dotenv.config();
 const app = express();
 app.use(cors());
